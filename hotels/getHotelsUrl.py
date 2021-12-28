4f7965f0e3e70c8bb5ab01c263903aebcac61514
hichembenh <hichem.benhamed@sesame.com.tn>
2021-12-28 17:58:06 +0100

    'hotelsScrapped'

--- hotels/getHotelsUrl.py
+++ hotels/getHotelsUrl.py
@@ -2,60 +2,79 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
 from time import sleep
-import csv
 import requests
 
-SCARP_URL = " https://fr.hotels.com/search.do?destination-id=10233175&q-check-in=2021-12-29&q-check-out=2021-12-30&q-rooms=1&q-room-0-adults=2&q-room-0-children=0&sort-order=BEST_SELLER"
+in_df = pd.read_csv('./links_to_extract.csv')
+
 out_df = pd.DataFrame()
 
-driver = webdriver.chrome.webdriver.WebDriver(executable_path=r'C:/Users/21655/Desktop/chromedriver')
 print('opening drive')
 
-search_list_to_prase_link = requests.get(str(SCARP_URL))
+for i in in_df.index:
+    hotel_names = []
+    hotel_price = []
+    hotel_ratings = []
+    total_reviews = []
+    hotel_url = []
+    location = []
+    country = []
+    print((i / len(in_df))*100,'% ...')
+    print('Working on', in_df['Country'][i], "hotels...")
+    link = requests.get(in_df['Link_to_hotels'][i])
+    if link.status_code == 200:
+        driver = webdriver.chrome.webdriver.WebDriver(executable_path=r'C:/Users/21655/Desktop/chromedriver')
+        driver.get(str(in_df['Link_to_hotels'][i]))
+        print("Scrolling ... ")
+        itheration = 0
+        while itheration <= 50:
+            # Scroll down to bottom
+            print("Iteration", itheration + 1)
+            scrolling = driver.execute_script("return document.body.scrollHeight") - 1200
+            print('Scrolling', scrolling)
+            driver.execute_script("window.scrollTo(0," + str(scrolling) + ")")
+            # Wait to load page
+            sleep(8)
+            itheration += 1
+        print('Getting the web page')
+        web_page = driver.page_source
+        soup = BeautifulSoup(web_page, 'lxml')
+        # Filtering the wanted tags from the fetched html
+        hotelContainer = soup.find_all("div", {"class": "-RcIiD"})
+        for hotel in hotelContainer:
+            hotelName = hotel.find("h2", {"class": "_3zH0kn"})
+            hotelPrice = hotel.find("span", {"class": "_2R4dw5"})
+            hotelRating = hotel.find("span", {"class": "_1biq31 _11XjrQ _3yXMS-"})
+            totalReview = hotel.find("span", {"class": "_2jUNPN"})
+            loc = hotel.find("p", {"class": "_2oHhXM"})
+            hotelUrl = hotel.find("a", {"class": "_61P-R0"})
+            if hotelRating is None or totalReview is None or loc is None:
+                print('Passing hotel with no rating...')
+                continue
+            hotel_names.append(hotelName.text)
+            country.append(in_df['Country'][i])
+            hotel_price.append(hotelPrice.text)
+            hotel_ratings.append(hotelRating.text)
+            total_reviews.append(totalReview.text)
+            location.append(loc.text)
+            hotel_url.append(hotelUrl['href'])
+        my_df = pd.DataFrame()
+        my_df['country'] = country
+        my_df['hotel_name'] = hotel_names
+        my_df['price'] = hotel_price
+        my_df['hotel_ratings'] = hotel_ratings
+        my_df['total_reviews'] = total_reviews
+        my_df['location'] = location
+        my_df['hotel_url'] = hotel_url
+        out_df = out_df.append(my_df, ignore_index=True)
+        print("waiting 5 sec")
+        sleep(5)
+        print("Closing driver")
+        driver.close()
+        driver.quit()
+        print(out_df)
+    else:
+        pass
 
-hotel_names = []
-hotel_price = []
-
-if search_list_to_prase_link.status_code == 200:
-    driver.get(search_list_to_prase_link.url)
-    print("Scrolling ... ")
-    scrolling = 2000
-    estimated_pages = 10
-    i = 0
-    while i <= 20:
-        # Scroll down to bottom
-        print(i + 1, "iteration")
-        print('Scrolling:', scrolling)
-        driver.execute_script("window.scrollTo(0," + str(scrolling) + ")")
-        if 2 <= i <= 4:
-            scrolling += 2600
-        elif 5 <= i:
-            scrolling += 2800
-        else:
-            scrolling += 2300
-        # Wait to load page
-        sleep(15)
-        i += 1
-
-    sleep(10)
-
-    print('Getting the web page')
-    web_page = driver.page_source
-    soup = BeautifulSoup(web_page, 'lxml')
-    # Filtering the wanted tags from the fetched html
-    hotelNames = soup.find_all("h2", {"class": "_3zH0kn"})
-    hotelPrice = soup.find_all("span", {"class": "_2R4dw5"})
-    for hotelName in hotelNames:
-        hotel_names.append(hotelName.text)
-    print(len(hotel_names))
-else:
-    pass
-
-print("waiting 5 sec")
-sleep(5)
-print("Closing driver")
-driver.close()
-driver.quit()
-
+print("Saving file...")
 # Saving all the  data in a df
-# out_df.to_csv("links_out.csv", encoding='utf-8')
+out_df.to_csv("links_out.csv", encoding='utf-8')
